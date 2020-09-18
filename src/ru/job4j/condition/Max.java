8767235b5a0a95c545fd5e350139b9952ce42435
devvk <info@sekretov.net>
2020-09-18 13:02:55 +0200

    4.2. Тернарное сравнение [#352859]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Max {
+    /*
+    Добавьте еще тест-методы с проверками:
+        первое максимальное,
+        второе максимальное,
+        оба числа одинаковые
+        (если оба одинаковые, то вернуть любое из этих чисел).
+     */
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+}
