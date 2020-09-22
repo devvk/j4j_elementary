32c06fde0cb358c310a121eff2b62c9a52527219
devvk <info@sekretov.net>
2020-09-22 11:35:44 +0200

    12. Необычные пары

---
+++ src/ru/job4j/condition/PairsCharString.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class PairsCharString {
+    public static boolean check(String l, String r) {
+        boolean check = false;
+        if (l.equals("")) {
+            check = true;
+        } else if( (l.charAt(0) == r.charAt(r.length() - 1) && r.charAt(0) == l.charAt(l.length() - 1)) ) {
+            check = true;
+        }
+        return check;
+    }
+}
