1bc673c0d9318cfe3c4f7c802fc185d2f27000f2
devvk <info@sekretov.net>
2020-09-23 18:52:37 +0200

    5.9.1. Генерация конструкций в IDEA [#352961]

---
+++ src/ru/job4j/live/Main.java
@@ -0,0 +1,12 @@
+package ru.job4j.live;
+
+public class Main {
+    // psvm
+    public static void main(String[] args) {
+        // fori
+        for (int i = 0; i <= 10; i++) {
+            // sout
+            System.out.println(i);
+        }
+    }
+}
