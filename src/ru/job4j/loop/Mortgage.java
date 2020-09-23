5462f9ee3363b0e8f98f867803883358dd6d4832
devvk <info@sekretov.net>
2020-09-23 17:09:43 +0200

    5.7. Ипотека [#352956]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    /*
+    Метод должен вычислять количество лет, необходимых для погашения кредита.
+    amount - сумма выданная по кредиту,
+    salary - годовой доход,
+    percent - процентная ставка по кредиту.
+     */
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * (int)percent / 100 - salary;
+            year++;
+        }
+        return year;
+    }
+}
