6e7aa2a50ee7a207a8631a3a787d2ccf3d419936
devvk <info@sekretov.net>
2020-09-25 11:22:58 +0200

    6.0.2. Заполнение массива. [#352880]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива 'ages' равен: " + ages.length);
         System.out.println("Размер массива 'surnames' равен: " + surnames.length);
         System.out.println("Размер массива 'prices' равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "John Lennon";
+        names[2] = "Kurt Cobain";
+        names[3] = "Wolfgang Amadeus Mozart";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
