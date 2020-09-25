1f2af866794eea73ba8e9499a82a5096880e9059
devvk <info@sekretov.net>
2020-09-25 11:03:03 +0200

    6.0.1. Объявление массива. [#352894]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        // тип_данных[] имя_переменной = new тип_данных[количество_ячеек];
+        short[] ages = new short[100];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
