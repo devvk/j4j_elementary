0905d70f195eb1042dfa2ea2ba0b1c78f4cf6438
devvk <info@sekretov.net>
2020-10-09 14:05:17 +0200

    18. Суммирования предыдущих значений

---
+++ src/ru/job4j/array/SummingPrevious.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class SummingPrevious {
+    public static int[] calculate(int a, int b, int n) {
+        int sum = 0;
+        int[] result  = new int[n];
+        result[0] = a;
+        result[1] = b;
+
+        for (int i = 2; i < result.length; i++) {
+            for (int j = 0; j < i; j++) {
+                sum += result[j];
+            }
+            result[i] = sum;
+            sum = 0;
+        }
+        return result;
+    }
+}
