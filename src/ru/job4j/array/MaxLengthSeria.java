88a4014b53384b9a081970300c5a4c7f5b6c114e
devvk <info@sekretov.net>
2020-10-13 10:48:41 +0200

    22. Максимальная длина неубывающей последовательности

---
+++ src/ru/job4j/array/MaxLengthSeria.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class MaxLengthSeria {
+    public static int find(int[] array) {
+        int length = 1;
+
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] >= array[i - 1]) {
+                    length++;
+                } else {
+                    length = 1;
+                }
+            }
+        }
+        return length;
+    }
+}
