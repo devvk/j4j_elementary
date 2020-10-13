9e6a19bdc6ee32d96942ca89def48f8bfbec862c
devvk <info@sekretov.net>
2020-10-13 11:32:02 +0200

    23. Переставить две строки в матрице

---
+++ src/ru/job4j/array/SwapRows.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class SwapRows {
+    public static void swap(int[][] data, int src, int dst) {
+        int[] tmp;
+        tmp = data[src];
+        data[src] = data[dst];
+        data[dst] = tmp;
+    }
+}
+
