c2d4bc3886e1a09b89c35f3f27b9a22796bfafd1
devvk <info@sekretov.net>
2020-10-13 11:49:10 +0200

    24. Переставить два столбца в матрице

---
+++ src/ru/job4j/array/SwapCols.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class SwapCols {
+    public static void swap(int[][] data, int src, int dst) {
+        for (int i = 0; i < data.length; i++) {
+            int tmp;
+            tmp = data[i][src];
+            data[i][src] = data[i][dst];
+            data[i][dst] = tmp;
+        }
+    }
+}
