2daa15d323750b9db5b2c469f1c3747d63129b4d
devvk <info@sekretov.net>
2022-01-27 21:49:13 +0100

    1.3. mod через while

--- src/ru/job4j/loop/DigitSum.java
+++ src/ru/job4j/loop/DigitSum.java
@@ -1,11 +1,22 @@
 package ru.job4j.loop;
 
 public class DigitSum {
+    public static void main(String[] args) {
+        System.out.println(sum(1));         // 1
+        System.out.println(sum(12));        // 3
+        System.out.println(sum(123));       // 6
+        System.out.println(sum(1234));      // 10
+        System.out.println(sum(12345));     // 15
+        System.out.println(sum(45));        // 9
+    }
+
     public static int sum(int num) {
         int sum = 0;
-        String number = Integer.toString(num);
-        for (int i = 1; i <= number.length(); i++) {
-            sum += i;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+            System.out.println("sum: " + sum);
+            System.out.println("num: " + num);
         }
         return sum;
     }
