043db51eaed73b31164d32e15bae0518053d91e9
devvk <info@sekretov.net>
2022-02-22 21:38:52 +0100

    Сортировка выбором

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,6 +2,13 @@ package ru.job4j.array;
 
 public class SortSelected {
     // data - массив чисел, который нужно отсортировать по возрастанию.
+
+    /**
+     * Сортировка выбором.
+     *
+     * @param data Массив чисел, который нужно отсортировать по возрастанию.
+     * @return Отсортированный массив.
+     */
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
