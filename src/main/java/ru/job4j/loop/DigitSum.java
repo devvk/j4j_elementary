cf7420b2386ac1271d8fc40400fa8c3f028ac9a3
devvk <info@sekretov.net>
2022-02-22 21:48:56 +0100

    1.4. Сумма цифр

--- src/main/java/ru/job4j/loop/DigitSum.java
+++ src/main/java/ru/job4j/loop/DigitSum.java
@@ -2,12 +2,12 @@ package ru.job4j.loop;
 
 public class DigitSum {
     public static void main(String[] args) {
-        System.out.println(sum(1));         // 1
-        System.out.println(sum(12));        // 3
-        System.out.println(sum(123));       // 6
-        System.out.println(sum(1234));      // 10
-        System.out.println(sum(12345));     // 15
-        System.out.println(sum(45));        // 9
+        System.out.println(sum(1));
+        System.out.println(sum(12));
+        System.out.println(sum(123));
+        System.out.println(sum(1234));
+        System.out.println(sum(12345));
+        System.out.println(sum(45));
     }
 
     /**
