c4c9fabb24f91a85a9621b7bcc0ba10a6d76af06
devvk <info@sekretov.net>
2022-02-23 16:34:14 +0100

    6.1. Классический поиск перебором. [#33489 #142797]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,8 +1,15 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    /**
+     * Поиск элемента в массиве.
+     *
+     * @param data Массив чисел.
+     * @param el   Элемент, который необходимо найти.
+     * @return Индекс найденного элемента, либо -1.
+     */
     public static int indexOf(int[] data, int el) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
@@ -12,14 +19,17 @@ public class FindLoop {
         return rst;
     }
 
-    /*
-    data    - массив чисел,
-    el      - элемент, который нужно найти,
-    start   - индекс, с которого начинаем поиск,
-    finish  - индекс, которым заканчиваем поиск.
+    /**
+     * Поиск элемента в массиве.
+     *
+     * @param data   Массив чисел.
+     * @param el     Элемент, который необходимо найти.
+     * @param start  Индекс, с которого начинается поиск.
+     * @param finish Индекс, которым заканчивается поиск.
+     * @return Индекс найденного элемента, либо -1.
      */
     public static int indexOf(int[] data, int el, int start, int finish) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1;
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
