ed8aa5f1f4d6f0e6cd0d280241e522677d178f38
devvk <info@sekretov.net>
2022-05-04 22:21:33 +0200

    Дефрагментация массива [#225 #142812]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -2,12 +2,15 @@ package ru.job4j.array;
 
 public class Defragment {
     /**
-     * Переместить заполненные ячейки массива в начало, а пустые в конец
+     * Дефрагментация массива.
+     * Переместить заполненные ячейки массива в начало, а пустые в конец.
+     *
+     * @param array неотсортированный массив
+     * @return дефрагментированный массив
      */
     public static String[] compress(String[] array) {
         for (int nullIndex = 0; nullIndex < array.length; nullIndex++) {
             if (array[nullIndex] == null) {
-                /* переместить первую не null ячейку.  */
                 for (int notNullIndex = nullIndex; notNullIndex < array.length; notNullIndex++) {
                     if (array[notNullIndex] != null) {
                         String tmp = array[notNullIndex];
@@ -20,14 +23,5 @@ public class Defragment {
             System.out.print(array[nullIndex] + " ");
         }
         return array;
-    }
-
-    public static void main(String[] args) {
-        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
-        String[] compressed = compress(input);
-        System.out.println();
-        for (String s : compressed) {
-            System.out.print(s + " ");
-        }
     }
 }
