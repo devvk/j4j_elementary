855e76a1613a2c608731a520f06b67b89614c93f
devvk <info@sekretov.net>
2022-05-05 23:11:23 +0200

    0.3. Объединить два отсортированных массива

--- src/main/java/ru/job4j/array/Merge.java
+++ src/main/java/ru/job4j/array/Merge.java
@@ -1,22 +1,29 @@
 package ru.job4j.array;
 
 /**
- * Сортировка слиянием
+ * Сортировка слиянием.
  */
 public class Merge {
+    /**
+     * Объединить два отсортированных массива.
+     *
+     * @param left  первый массив
+     * @param right второй массив
+     * @return отсортированный массив
+     */
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int countLeft = 0;
-        int countRight = 0;
+        int countL = 0;
+        int countR = 0;
         for (int i = 0; i < rsl.length; i++) {
-            if (countLeft == left.length) {
-                rsl[i] = right[countRight++];
-            } else if (countRight == right.length) {
-                rsl[i] = left[countLeft++];
-            } else if (left[countLeft] < right[countRight]) {
-                rsl[i] = left[countLeft++];
+            if (countL == left.length) {
+                rsl[i] = right[countR++];
+            } else if (countR == right.length) {
+                rsl[i] = left[countL++];
+            } else if (left[countL] < right[countR]) {
+                rsl[i] = left[countL++];
             } else {
-                rsl[i] = right[countRight++];
+                rsl[i] = right[countR++];
             }
         }
         return rsl;
