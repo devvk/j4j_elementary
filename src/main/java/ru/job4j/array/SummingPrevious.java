963a2a149f9f6ac2ea6c4570658bc174905b5835
devvk <info@sekretov.net>
2022-05-10 16:21:04 +0200

    1.9. Суммирования предыдущих значений

--- src/main/java/ru/job4j/array/SummingPrevious.java
+++ src/main/java/ru/job4j/array/SummingPrevious.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class SummingPrevious {
     public static int[] calculate(int a, int b, int n) {
         int sum = 0;
-        int[] result  = new int[n];
+        int[] result = new int[n];
         result[0] = a;
         result[1] = b;
 
--- src/test/java/ru/job4j/array/SummingPreviousTest.java
+++ src/test/java/ru/job4j/array/SummingPreviousTest.java
@@ -2,15 +2,15 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class SummingPreviousTest {
 
     @Test
     public void test() {
-        assertArrayEquals(new int[] {1, 2, 3}, SummingPrevious.calculate(1, 2, 3));
-        assertArrayEquals(new int[] {1, 2, 3, 6}, SummingPrevious.calculate(1, 2, 4));
-        assertArrayEquals(new int[] {1, 2, 3, 6, 12}, SummingPrevious.calculate(1, 2, 5));
+        assertArrayEquals(new int[]{1, 2, 3}, SummingPrevious.calculate(1, 2, 3));
+        assertArrayEquals(new int[]{1, 2, 3, 6}, SummingPrevious.calculate(1, 2, 4));
+        assertArrayEquals(new int[]{1, 2, 3, 6, 12}, SummingPrevious.calculate(1, 2, 5));
     }
 
 }
