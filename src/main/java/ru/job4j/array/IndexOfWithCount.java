151dcc190eb46227e1f559598c0b71ada629bae2
devvk <info@sekretov.net>
2022-05-10 17:21:30 +0200

    2.3. indexOf с числом вхождений

--- src/main/java/ru/job4j/array/IndexOfWithCount.java
+++ src/main/java/ru/job4j/array/IndexOfWithCount.java
@@ -3,14 +3,14 @@ package ru.job4j.array;
 public class IndexOfWithCount {
     public static int indexOf(char[] string, char c, int number) {
         int result = -1;
-        int n = 0;
+        int counter = 0;
         for (int i = 0; i < string.length; i++) {
             if (string[i] == c) {
-                n++;
-                if (n == number) {
+                counter++;
+                if (counter == number) {
                     result = i;
+                    break;
                 }
-
             }
         }
         return result;
