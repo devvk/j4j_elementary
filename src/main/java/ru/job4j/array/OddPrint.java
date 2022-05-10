c1da00aa05e285225779f440439a8892557531ee
devvk <info@sekretov.net>
2022-05-10 16:33:19 +0200

    2.1. Вывести нечетные в порядке возрастания индексов

--- src/main/java/ru/job4j/array/OddPrint.java
+++ src/main/java/ru/job4j/array/OddPrint.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class OddPrint {
     public static void print(int[] array) {
         for (int i : array) {
-            if ((i % 2) != 0) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
