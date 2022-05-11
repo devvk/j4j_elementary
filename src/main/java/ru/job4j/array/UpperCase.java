83b3c0400f3c99a1c2c047dd011967a899b85ac5
devvk <info@sekretov.net>
2022-05-11 22:25:19 +0200

    2.9. Привести строку к верхнему регистру

--- src/main/java/ru/job4j/array/UpperCase.java
+++ src/main/java/ru/job4j/array/UpperCase.java
@@ -3,9 +3,7 @@ package ru.job4j.array;
 public class UpperCase {
     public static char[] toUpperCase(char[] string) {
         for (int i = 0; i < string.length; i++) {
-            if (Character.isLowerCase(string[i])) {
-                string[i] = Character.toUpperCase(string[i]);
-            }
+            string[i] = Character.isLowerCase(string[i]) ? Character.toUpperCase(string[i]) : string[i];
         }
         return string;
     }
