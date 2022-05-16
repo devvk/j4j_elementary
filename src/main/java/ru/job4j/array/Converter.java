4fd803d60d0829a1caad43af72d1ebc34f4a90fc
devvk <info@sekretov.net>
2022-05-16 22:18:34 +0200

    3.4. Конвертировать неквадратный двумерный массив в квадратный.

--- src/main/java/ru/job4j/array/Converter.java
+++ src/main/java/ru/job4j/array/Converter.java
@@ -3,28 +3,30 @@ package ru.job4j.array;
 public class Converter {
     public static int[][] convertInSquareArray(int[][] array) {
         int countArrayElements = 0;
-        int squareArraySize = 0;
         for (int[] ints : array) {
             for (int j = 0; j < ints.length; j++) {
                 countArrayElements++;
             }
         }
+
+        int squareArraySize = 0;
         for (int i = 0; i < countArrayElements; i++) {
             if (i * i >= countArrayElements) {
                 squareArraySize = i;
                 break;
             }
         }
+
+        int[][] squareArray = new int[squareArraySize][squareArraySize];
         int x = 0;
         int y = 0;
-        int[][] squareArray = new int[squareArraySize][squareArraySize];
         for (int[] ints : array) {
-            for (int anInt : ints) {
+            for (int i : ints) {
                 if (x >= squareArray.length) {
                     y++;
                     x = 0;
                 }
-                squareArray[y][x] = anInt;
+                squareArray[y][x] = i;
                 x++;
             }
         }
