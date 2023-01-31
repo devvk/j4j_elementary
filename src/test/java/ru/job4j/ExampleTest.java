6ca936e6f2054fa4e0d2d8addb32baf749dcdb1f
devvk <info@sekretov.net>
2023-01-31 19:32:01 +0100

    6. DSL c AssertJ [#504879 #386924]

---
+++ src/test/java/ru/job4j/ExampleTest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class ExampleTest {
+
+    @Test
+    public void whenEquals() {
+        int result = 1;
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFloatPoint() {
+        float result = 1.0000001F;
+        float expected = 1F;
+        assertThat(result).isCloseTo(expected, offset(0.01F));
+    }
+
+    @Test
+    public void whenArray() {
+        int[] result = {1, 2, 3};
+        int[] expected = {1, 2, 3};
+        assertThat(result).containsExactly(expected);
+    }
+}
