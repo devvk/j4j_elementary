37a04357f74f78125d064cadb4b8b4378272112c
devvk <info@sekretov.net>
2023-02-02 15:12:25 +0100

    5.4. Протеиновая диета (цикл while) [#156313 #142882]

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -1,32 +1,43 @@
 package ru.job4j.loop;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FitnessTest {
     @Test
     public void whenIvanGreatNik() {
-        int month = Fitness.calc(95, 90);
-        assertThat(month, is(0));
+        int ivan = 95;
+        int nik = 90;
+        int result = Fitness.calc(ivan, nik);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenIvanLessByOneNik() {
-        int month = Fitness.calc(90, 95);
-        assertThat(month, is(1));
+        int ivan = 90;
+        int nik = 95;
+        int result = Fitness.calc(ivan, nik);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenIvanLessByFewNik() {
-        int month = Fitness.calc(50, 90);
-        assertThat(month, is(2));
+        int ivan = 50;
+        int nik = 90;
+        int result = Fitness.calc(ivan, nik);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenIvanEqualNik() {
-        int month = Fitness.calc(90, 90);
-        assertThat(month, is(1));
+    public void whenIvanEqualsNik() {
+        int ivan = 90;
+        int nik = 90;
+        int result = Fitness.calc(ivan, nik);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
     }
 }
