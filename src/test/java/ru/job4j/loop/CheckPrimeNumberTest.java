bfd1c21a838156aad0124d633b7c30110e78616f
devvk <info@sekretov.net>
2023-02-02 15:58:22 +0100

    5.5. Простое число [#171694 #142881]

--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -7,19 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CheckPrimeNumberTest {
     @Test
     public void when5() {
-        boolean rsl = CheckPrimeNumber.check(5);
-        assertThat(rsl).isTrue();
+        int number = 5;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
     }
 
     @Test
     public void when4() {
-        boolean rsl = CheckPrimeNumber.check(4);
-        assertThat(rsl).isFalse();
+        int number = 4;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 
     @Test
     public void when1() {
-        boolean rsl = CheckPrimeNumber.check(1);
-        assertThat(rsl).isFalse();
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 }
