ef4a37317b4e226d98df1e512b4e8e8b1d7c04af
devvk <info@sekretov.net>
2023-02-03 18:13:04 +0100

    6.2.3. Перевернуть массив. [#226790 #142805]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -1,12 +1,18 @@
 package ru.job4j.array;
 
 public class Turn {
+    /**
+     * Метод переставляет все элементы массива в обратном порядке.
+     *
+     * @param array Массив с данными.
+     * @return Массив с переставленными элементами.
+     */
     public static int[] back(int[] array) {
         int tmp;
         for (int i = 0; i < array.length / 2; i++) {
             tmp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = tmp;
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
         }
         return array;
     }
--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -1,17 +1,20 @@
 package ru.job4j.array;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TurnTest {
+
+    /**
+     * Тест проверяет переворот массива с чётным числом элементов.
+     */
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
-        int[] input = new int[]{4, 1, 6, 2};
-        int[] result = Turn.back(input);
-        int[] expect = new int[]{2, 6, 1, 4};
-        assertThat(result, is(expect));
+        int[] data = new int[]{4, 1, 6, 2};
+        int[] result = Turn.back(data);
+        int[] expected = new int[]{2, 6, 1, 4};
+        assertThat(result).containsExactly(expected);
     }
 
     /**
@@ -19,9 +22,9 @@ public class TurnTest {
      */
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        int[] input = new int[]{1, 2, 3, 4, 5};
-        int[] result = Turn.back(input);
-        int[] expect = new int[]{5, 4, 3, 2, 1};
-        assertThat(result, is(expect));
+        int[] data = new int[]{1, 2, 3, 4, 5};
+        int[] result = Turn.back(data);
+        int[] expected = new int[]{5, 4, 3, 2, 1};
+        assertThat(result).containsExactly(expected);
     }
 }
