109b1ab06fcffd905add4bc9ed1a5563c80fe015
devvk <info@sekretov.net>
2023-02-03 16:45:59 +0100

    6.1. Классический поиск перебором. [#33489 #142797]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,36 +1,37 @@
 package ru.job4j.array;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        int[] input = new int[]{5, 4, 3, 2};
+        int[] data = new int[]{5, 4, 3, 2};
         int value = 5;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(data, value);
         int expected = 0;
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenFind3() {
-        int[] input = new int[]{5, 2, 10, 2, 4};
+        int[] data = new int[]{5, 2, 10, 2, 4};
         int value = 2;
         int start = 2;
         int finish = 4;
-        int result = FindLoop.indexOf(input, value, start, finish);
+        int result = FindLoop.indexOf(data, value, start, finish);
         int expected = 3;
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenArrayHasNot10ThenMinus1() {
-        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] data = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         int value = 10;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(data, value);
         int expected = -1;
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 }
