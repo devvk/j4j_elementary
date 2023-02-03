52c65243d2a6ad2dd38a77d360a42624f8110741
devvk <info@sekretov.net>
2023-02-03 15:58:30 +0100

    6.0.7. Массивы в тестах. [#226789 #142803]

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -9,15 +9,15 @@ public class SquareTest {
     public void whenBound3Then014() {
         int bound = 3;
         int[] result = Square.calculate(bound);
-        int[] expect = new int[]{0, 1, 4};
-        assertThat(result).containsExactly(expect);
+        int[] expected = new int[]{0, 1, 4};
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
     public void whenBound5Then014916() {
         int bound = 5;
         int[] result = Square.calculate(bound);
-        int[] expect = new int[]{0, 1, 4, 9, 16};
-        assertThat(result).containsExactly(expect);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
     }
 }
