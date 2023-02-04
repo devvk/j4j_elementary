e89ad1315d04372bb21630610625cb04061c48e6
devvk <info@sekretov.net>
2023-02-04 11:21:12 +0100

    6.4.3. Слово заканчивается на ... [#156317 #142818]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -1,9 +1,15 @@
 package ru.job4j.array;
 
 public class EndsWith {
+    /**
+     * Метод проверяет то, что слово заканчивается на определенную последовательность.
+     *
+     * @param word проверяемый массив
+     * @param post последовательность
+     * @return результат проверки true или false
+     */
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        /* проверить. что массив word имеет последние элементы одинаковые с post */
         for (int i = 1; i < post.length && i < word.length; i++) {
             if (post[post.length - i] != word[word.length - i]) {
                 result = false;
--- src/test/java/ru/job4j/array/EndsWithTest.java
+++ src/test/java/ru/job4j/array/EndsWithTest.java
@@ -1,9 +1,8 @@
 package ru.job4j.array;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class EndsWithTest {
 
@@ -12,7 +11,7 @@ public class EndsWithTest {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
         char[] post = {'l', 'o'};
         boolean result = EndsWith.endsWith(word, post);
-        assertThat(result, is(true));
+        assertThat(result).isTrue();
     }
 
     @Test
@@ -20,6 +19,6 @@ public class EndsWithTest {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
         char[] post = {'l', 'a'};
         boolean result = EndsWith.endsWith(word, post);
-        assertThat(result, is(false));
+        assertThat(result).isFalse();
     }
 }
