071d2be157fe58ac7891fa226dc91d88eed00031
devvk <info@sekretov.net>
2023-02-07 12:37:14 +0100

    6.6.4. Двухмерный массив. Таблица умножения. [#33491 #142794]

--- src/test/java/ru/job4j/array/MatrixTest.java
+++ src/test/java/ru/job4j/array/MatrixTest.java
@@ -1,38 +1,40 @@
 package ru.job4j.array;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixTest {
     @Test
     public void when2on2() {
-        int[][] table = Matrix.multiple(2);
-        int[][] expect = {
+        int size = 2;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
                 {1, 2},
                 {2, 4}
         };
-        assertThat(table, is(expect));
+        assertThat(result).isDeepEqualTo(expected);
     }
 
     @Test
     public void when5on5() {
-        int[][] table = Matrix.multiple(5);
-        int[][] expect = {
+        int size = 5;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
                 {1, 2, 3, 4, 5},
                 {2, 4, 6, 8, 10},
                 {3, 6, 9, 12, 15},
                 {4, 8, 12, 16, 20},
                 {5, 10, 15, 20, 25}
         };
-        assertThat(table, is(expect));
+        assertThat(result).isDeepEqualTo(expected);
     }
 
     @Test
     public void when10on10() {
-        int[][] table = Matrix.multiple(10);
-        int[][] expect = {
+        int size = 10;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
                 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
                 {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
                 {3, 6, 9, 12, 15, 18, 21, 24, 27, 30},
@@ -44,6 +46,6 @@ public class MatrixTest {
                 {9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
                 {10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
         };
-        assertThat(table, is(expect));
+        assertThat(result).isDeepEqualTo(expected);
     }
 }
